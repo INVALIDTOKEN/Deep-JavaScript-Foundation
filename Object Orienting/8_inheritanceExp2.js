3c2a84f0d41b833b95137ec22f80f9138ae67301
INVALIDTOKEN <codeit.sahil98@gmail.com>
2018-07-06 18:21:52 +0530

    added IIFI arguments

--- Object Orienting/8_inheritanceExp2.js
+++ Object Orienting/8_inheritanceExp2.js
@@ -2,7 +2,7 @@ function throwError(parameterName){
   throw new Error("Provide argument " + parameterName);
 }
 
-function Particle({ x = (throwError)(), y = (throwError)()}={}){
+function Particle({ x = (throwError)("x"), y = (throwError)("y")}={}){
   this._x = x;
   this._y = y;
 }
@@ -14,7 +14,7 @@ Particle.prototype.valueOfY = function(){
   return this._y;
 }
 
-function ColoredParticle({ x = (throwError)(), y = (throwError)(), color = (throwError)() }={}){
+function ColoredParticle({ x = (throwError)("x"), y = (throwError)("y"), color = (throwError)("color") }={}){
   Particle.call(this, {x , y});
   this._color = color;
 }
@@ -32,4 +32,4 @@ ColoredParticle.prototype.logDetails = function(){
 }
 
 let coloredParticle = new ColoredParticle({ x : 200, y : 400, color : "black"});
-console.log(coloredParticle);
+console.log(coloredParticle.logDetails());
